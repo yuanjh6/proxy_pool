39e5c063dfd5c8c28cb4eb2152d0c1f05f769410
yuanjh6 <553882335@163.com>
2023-11-02 01:05:42 +0800

    modify

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+#source /home/john/miniconda3/etc/profile.d/conda.sh
+#conda activate proxy_pool_env
+#source ~/miniconda3/etc/profile.d/conda.sh
+#conda init
+#eval "$(conda shell.bash hook)"
+#source ~/.bashrc
+#conda activate proxy_pool_env
 python proxyPool.py server &
 python proxyPool.py schedule
